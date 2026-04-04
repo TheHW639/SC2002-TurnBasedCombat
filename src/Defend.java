e291625647a3e9cb32ff5a9fb5d0946136df84a1
Zhang Haoyuan <c250167@e.ntu.edu.sg>
2026-04-05 00:28:09 +0800

    Create Defend.java

---
+++ src/Defend.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Defend implements Action {
+    @Override
+    public void execute(Combatant performer, List<Combatant> targets) {
+        performer.addEffect(new DefenseBuffEffect(2));
+        System.out.println(performer.getName() + " defends, defense increased for 2 turns.");
+    }
+
+    @Override
+    public String getName() {
+        return "Defend";
+    }
+}
